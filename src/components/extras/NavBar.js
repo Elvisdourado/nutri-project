ead5eeff2d7826f949373f8fc860027da92812c4
Elvis Dourado <elvisdourado@gmail.com>
2021-04-29 16:12:06 -0300

    Update NavBar.js

--- src/components/extras/NavBar.js
+++ src/components/extras/NavBar.js
@@ -2,19 +2,20 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav/'
 import MainLogo from '../assets/marca_NEG.png'
+import {Link} from 'react-router-dom'
 
 
 function NavBar() {
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="lg" sticky="top" >
-        <Navbar.Brand href="/"><img src={MainLogo} height='50px' width='150px' alt='logo' /> </Navbar.Brand>
+        <Link to="/"><img src={MainLogo} height='50px' width='150px' alt='logo' /> </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/FullSearch">Busca completa</Nav.Link>
-            <Nav.Link href="/About">Sobre</Nav.Link>
+            <Link className='btn btn-secondary m-1' style={{backgroundColor: "#343a40", border: "none"}} to="/">Home</Link>
+            <Link className='btn btn-secondary m-1' style={{backgroundColor: "#343a40", border: "none"}} to="/FullSearch">Busca completa</Link>
+            <Link className='btn btn-secondary m-1' style={{backgroundColor: "#343a40", border: "none"}} to="/About">Sobre</Link>
           </Nav>          
         </Navbar.Collapse>
       </Navbar>
